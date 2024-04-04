cbd126574945878ce95a431091f5d8e4140b53c2
rodrigoterradigital <rodrigoterradigital@gmail.com>
2024-04-04 10:32:09 -0300

    primeiro commit

---
+++ desafio01.rb
@@ -0,0 +1,14 @@
+# Pede para o usuário digitar o seu nome
+print "Digite seu nome: "
+nome = gets.chomp
+
+# Pede para o usuário digitar sobrenome
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+# Pede para o usuário digitar a sua idade
+print "Digite sua idade: "
+idade = gets.chomp
+
+# Mostra o que o usuário digitou
+puts "Oi #{nome} #{sobrenome}! Sua idade é #{idade} anos."
